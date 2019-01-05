4428773586bfab85a96abcbf3865e11b225fcc57
Ashley Achibald <aarchibald20@gmail.com>
2019-01-05 02:28:28 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,32 @@
+var chocolateBars = ["snickers" , "hundredgrand"
+ , "kitkat", "skittles"];
+
+ addElementToBeginningOfArray(chocolateBars, element){
+   return [element, ...chocolateBars]
+ }
+ destructivelyAddElementToBeginningOfArray(){
+   chocolateBars.unshift(element)
+   return chocolateBars
+ }
+ addElementToEndOfArray(chocolateBars, element){
+   return [...chocolateBars, element]
+ }
+ destructivelyAddElementToEndOfArray(chocolateBars, element){
+   array.push(element)
+   return array
+ }
+ destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
+   array.shift()
+   array
+ }
+ removeElementFromBeginningOfArray(chocolateBars){
+   array.slice(1)
+ }
+ destructivelyRemoveElementFromEndOfArray(){
+   array.pop()
+   return array
+ }
+ removeElementFromEndOfArray(chocolateBars){
+   return array.slice(0, array.length - 1)
+ }
+ 
